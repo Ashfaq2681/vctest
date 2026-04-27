c9011fae8897dfd7d4387856b489c5fba1963088
Ashfaq2681 <91347989+Ashfaq2681@users.noreply.github.com>
2026-04-27 18:47:10 +0500

    Add files via upload

---
+++ assets/Ideas-B-AUdUO4.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BsgHfewW.js";import{C as o}from"./ContentCollectionPage-BAQm4mCw.js";const a=()=>t.jsx(o,{type:"idea"});export{a as default};
---
+++ assets/News-CWYIuEfn.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BsgHfewW.js";import{C as o}from"./ContentCollectionPage-BAQm4mCw.js";const n=()=>t.jsx(o,{type:"news"});export{n as default};
